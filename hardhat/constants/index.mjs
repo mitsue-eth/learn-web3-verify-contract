9dc18c53bedec2d33963b03587fafc5b20f3fd61
mitsue-eth <mitsue.eth@gmail.com>
2022-08-09 07:39:09 +0200

    first commit

---
+++ hardhat/constants/index.mjs
@@ -0,0 +1,4 @@
+export const metadataCID = 'QmegbPCcWPttzyz6EjuM6QXY4RyDVcXkSYU6ooMnPMJfN4';
+export const imagesCID = 'QmQBHarz2WFczTjz5GnhjHrbUPDnB48W5BM2v2h6HbE1rZ';
+export const LW3PunksContractAddress =
+	'0xac2D6A55B3748667A1D2B8431d41A7F3d2ac3b86';
